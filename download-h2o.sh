634b2e990abe724ebf7018dd698d0bad897c1fe9
ankrt <ankrt@mail.com>
2018-03-11 12:23:04 +0000

    Add .gitignore and download h2o to bin directory

--- download-h2o.sh
+++ download-h2o.sh
@@ -4,18 +4,22 @@ H2O="h2o-3.16.0.2"
 H2O_ZIP="$H2O.zip"
 H2O_URL="http://h2o-release.s3.amazonaws.com/h2o/rel-wheeler/2/$H2O_ZIP"
 
-MODEL_PATH="../DRF_model_R_1519552472385_2"
+BIN="./bin"
+MODEL_PATH="$BIN/DRF_model_R_1519552472385_2"
 
 # Download and unzip H20
 
-if [ ! -f ./$H2O_ZIP ]; then
-    wget "$H2O_URL" --show-progress > /dev/null
-    unzip "./$H2O_ZIP"
+if [ ! -f $BIN/$H2O_ZIP ]; then
+    wget -P $BIN "$H2O_URL" --show-progress > /dev/null
+fi
+
+if [ ! -f $BIN/$H2O/ ]; then
+    unzip "$BIN/$H2O_ZIP" -d "$BIN/$H2O/"
 fi
 
 # Run h2o in the background and upload the model to it
 
-java -jar ./$H2O/h2o.jar &>/dev/null &
+java -jar $BIN/$H2O/h2o.jar &>/dev/null &
 
 # Wait for h2o to start up...
 
